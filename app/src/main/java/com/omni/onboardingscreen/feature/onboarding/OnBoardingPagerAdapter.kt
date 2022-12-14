8b6879c53271d991234979f2111a096b9cf194b7
OmneyaHassanOsman <omneya.osman@thirdwayv.com>
2022-12-15 01:21:45 +0200

    make viewHolder inner class

--- app/src/main/java/com/omni/onboardingscreen/feature/onboarding/OnBoardingPagerAdapter.kt
+++ app/src/main/java/com/omni/onboardingscreen/feature/onboarding/OnBoardingPagerAdapter.kt
@@ -1,18 +1,16 @@
 package com.omni.onboardingscreen.feature.onboarding
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
-import com.omni.onboardingscreen.R
-import com.omni.onboardingscreen.databinding.ActivityMainBinding
 import com.omni.onboardingscreen.databinding.OnboardingPageItemBinding
-import com.omni.onboardingscreen.feature.onboarding.PagerViewHolder.Companion.LAYOUT
 import com.omni.onboardingscreen.feature.onboarding.entity.OnBoardingPage
 
+/**
+ *OnBoardingPagerAdapter adapter for the viewpager2
+ *  @param onBoardingPageList as Array */
 class OnBoardingPagerAdapter(private val onBoardingPageList: Array<OnBoardingPage> = OnBoardingPage.values()) :
-    RecyclerView.Adapter<PagerViewHolder>() {
+    RecyclerView.Adapter<OnBoardingPagerAdapter.PagerViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, p1: Int): PagerViewHolder =
@@ -28,20 +26,17 @@ class OnBoardingPagerAdapter(private val onBoardingPageList: Array<OnBoardingPag
     override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
         holder.bind(onBoardingPageList[position])
     }
-}
 
-class PagerViewHolder(private val binding: OnboardingPageItemBinding) :
-    RecyclerView.ViewHolder(binding.root) {
-    fun bind(onBoardingPage: OnBoardingPage) {
-        val res = binding.root.context.resources
-        binding.titleTv.text = res.getString(onBoardingPage.titleResource)
-        binding.subTitleTv.text = res.getString(onBoardingPage.subTitleResource)
-        binding.descTV.text = res.getString(onBoardingPage.descriptionResource)
-        binding.img.setImageResource(onBoardingPage.logoResource)
-    }
 
-    companion object {
-        @LayoutRes
-        val LAYOUT = R.layout.onboarding_page_item
+  inner  class PagerViewHolder(private val binding: OnboardingPageItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(onBoardingPage: OnBoardingPage) {
+            val res = binding.root.context.resources
+            binding.titleTv.text = res.getString(onBoardingPage.titleResource)
+            binding.subTitleTv.text = res.getString(onBoardingPage.subTitleResource)
+            binding.descTV.text = res.getString(onBoardingPage.descriptionResource)
+            binding.img.setImageResource(onBoardingPage.logoResource)
+        }
+
     }
 }
